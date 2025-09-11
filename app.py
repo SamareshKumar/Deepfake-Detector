75546f4b185a0c58cb893be4203f7ef712dffe7d
SAMARESH KUMAR <148460877+SamareshKumar@users.noreply.github.com>
2025-09-11 23:16:11 +0530

    Real-time detection of deepfake images

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca1b7fef2d8e12319c5a6b3b45b3e27ad8660faac9cb8b9bc4a670c5495e4457
-size 4561
+oid sha256:248956b89e5277497780344f3911e9a093f33e27099a817c61be52ba3bc34b5f
+size 4452
